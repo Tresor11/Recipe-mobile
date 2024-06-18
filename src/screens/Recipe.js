df67074b3752426bc44999160fb92774c77d389d
Tresor11 <bireketresor@gmail.com>
2024-06-18 20:15:07 +0200

    Add instructions to recipe screen
    This commit adds cooking instructions to the
    recipe screen.
    I also added the webview component to preview
    the youtube video of the recipe in the app.

--- src/screens/Recipe.js
+++ src/screens/Recipe.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Link} from 'react-native';
 import React, { useState, useEffect } from 'react';
+import { WebView } from 'react-native-webview';
 import Item from '../components/CategoryPreview';
 
 export default function Category({route, navigation}) {
@@ -25,7 +26,12 @@ export default function Category({route, navigation}) {
             <Image style={styles.image} source={{uri: menu[0].strMealThumb}}/>
           </View>
         <View>
-          <Text style={{fontWeight: 'bold', fontSize: '20rem'}}>{menu[0].strMeal}</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 20}}>{menu[0].strMeal}</Text>
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 15, marginTop: 20}}>Cooking instructions</Text>
+          <Text style={styles.instructions}>{menu[0].strInstructions}</Text>
+          <WebView source={{uri: menu[0].strYoutube}} style={{marginTop: 20, height: 200}}/>
         </View>
         </View>
       </ScrollView>
@@ -54,5 +60,11 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     height: 200,
     resizeMode: 'cover',
+  },
+  instructions: {
+    fontSize: 15,
+    marginTop: 10,
+    lineHeight: 25,
+    lineBreak: 'auto'
   }
 });
